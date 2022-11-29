20ffb7a85439a352e67afef21f7d2f485a14aed7
Gleb Shevchenko <shevzoom@gmail.com>
2022-11-29 22:39:16 +0300

    Initial Commit

---
+++ Ship/ShipApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShipApp.swift
+//  Ship
+//
+//  Created by Глеб on 29.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct ShipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
